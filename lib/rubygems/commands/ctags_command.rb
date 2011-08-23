16d4e86d71fdc84eb72ff7233e79833e43e126a1
Tim Pope <code@tpope.net>
2011-08-22 22:18:59 -0400

    Encapsulate ctags invocation

--- lib/rubygems/commands/ctags_command.rb
+++ lib/rubygems/commands/ctags_command.rb
@@ -7,11 +7,17 @@ class Gem::Commands::CtagsCommand < Gem::Command
 
   def execute
     Gem::Specification.each do |spec|
-      Dir.chdir(spec.full_gem_path) do
-        unless File.exist?('tags')
-          say "Generating ctags for #{spec.full_name}"
-          system('ctags', '-R', *spec.require_paths)
-        end
+      self.class.index(spec) do
+        say "Generating ctags for #{spec.full_name}"
+      end
+    end
+  end
+
+  def self.index(spec)
+    Dir.chdir(spec.full_gem_path) do
+      unless File.exist?('tags')
+        yield if block_given?
+        system('ctags', '-R', *spec.require_paths)
       end
     end
   end
--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -3,7 +3,6 @@ require 'rubygems/command_manager'
 Gem::CommandManager.instance.register_command :ctags
 
 Gem.post_install do |installer|
-  Dir.chdir(installer.spec.full_gem_path) do
-    system('ctags', '-R', *installer.spec.require_paths)
-  end
+  require 'rubygems/commands/ctags_command'
+  Gem::Commands::CtagsCommand.index(installer.spec)
 end
