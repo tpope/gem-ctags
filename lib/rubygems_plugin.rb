282745025447166f003d28ef0aacfabb034f16a4
Tim Pope <code@tpope.net>
2012-12-19 08:41:59 -0500

    Fix LoadError
    Closes #3.  I haven't narrowed down the exact trigger for this but the
    error message makes the solution pretty obvious.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,8 +1,8 @@
 require 'rubygems/command_manager'
+require 'rubygems/commands/ctags_command'
 
 Gem::CommandManager.instance.register_command :ctags
 
 Gem.post_install do |installer|
-  require 'rubygems/commands/ctags_command'
   Gem::Commands::CtagsCommand.index(installer.spec)
 end
